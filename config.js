35966a17b6eb386900bb6a8b1b71507713344f6b
Anjali <anjali@Anjalis-Air.lan>
2024-05-06 00:23:11 -0400

    final codet

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+module.exports = {
+  SECRET_KEY: 'anjali',
+  apiUrl: 'http://localhost:5000', 
+  mysql: {
+    host: 'sql5.freemysqlhosting.net',
+    user: 'sql5703439',
+    password: 'KcYQNuLp89',
+    database: 'sql5703439',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  },
+};
